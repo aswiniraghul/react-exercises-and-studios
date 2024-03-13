7f99a7eaf5aa4fd5aa2f1aa3182e5f76f72b9abb
Aswini Raghul <aswiniraghul@gmail.com>
2024-03-12 22:03:59 -0500

    completed React-part2 Exercises-2

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,41 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+const listItem = oceans.map(ocean =>
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <h1>{ocean.name}</h1>
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+      <h3>Ocean Facts:</h3>
+      <u1>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+      </u1>
+      <Button />
+   </div>   
+);
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+         <ul>
+            {listItem}
+         </ul>  
          <Button />
-      </>  
+      </>
    );
 }
-
 export default Profile;
+
+// function Profile()
+// {
+//    return(
+//       <>
+//          <Button />
+//       </>  
+//    );
+// }
+
+// export default Profile;
